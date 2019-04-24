18acb58e19650b93ee88c51125b7572938079a8f
Michael Chen <michaelchengaming@gmail.com>
2019-04-24 15:40:30 -0700

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,11 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |person, contact_datails_hash|
+    contact_datails_hash.each do |attribute, data|
+      if attribute == :favorite_ice_cream_flavors
+        data.delete_if {|flavor| flavor == "strawberry"}
+      end
+    end
+  end
 end
